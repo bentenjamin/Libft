37a376da8a75d7717d1bb6a172b68d4c57d04964
Benjamin WEBB <bwebb@c3r2s4.wethinkcode.co.za>
2019-05-24 15:53:26 +0200

    memcpy

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,41 +6,30 @@
 /*   By: bwebb <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 15:03:05 by bwebb             #+#    #+#             */
-/*   Updated: 2019/05/23 10:39:29 by bwebb            ###   ########.fr       */
+/*   Updated: 2019/05/24 15:53:08 by bwebb            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
-#include <string.h>
 
-void	*ft_memccpy(void *dst, void *sc, int c, size_t n)
+void	*ft_memccpy(void *dest, const void *source, int c, size_t n)
 {
 	int		i;
-	char	*fuck;
-	char	*pointers;
+	char	*dst;
+	char	*sc;
 
 	i = -1;
-	fuck = dst;
-	pointers = sc;
+	dst = dest;
+	sc = (char *) source;
 	if ((int) n > 0)
-        while ((++i < (int) n) && (pointers[i] != '\0'))
+        while ((++i < (int) n) && (sc[i] != '\0'))
 		{
-			if (pointers[(char) c] == pointers[i])
-				return(fuck + i + 1);
-			else
-				fuck[i] = pointers[i];
+			dst[i] = sc[i];
+			if ((unsigned char) c == sc[i])
+			{
+				dst[i+1] = '?';
+				return (dst + i + 1);
+			}
 		}
-	return(NULL);
-}
-
-int main () {
-   char src[50] = "http://www.tutorialspoint.com";
-   char dest[50];
-   strcpy(dest,"Heloooo!!");
-   printf("Before memcpy dest = %s\n", dest);
-   printf("%s", ft_memccpy(dest, src, 105, strlen(src)+1));
-   printf("After memcpy dest = %s\n", dest);
-
-   return(0);
+	return (NULL);
 }
