8b69353e848e72a200d732d4e992f9db48024cdc
Benjamin WEBB <bwebb@c3r2s3.wethinkcode.co.za>
2019-05-27 13:34:39 +0200

    memchr fix

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: bwebb <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 16:11:05 by bwebb             #+#    #+#             */
-/*   Updated: 2019/05/24 17:06:18 by bwebb            ###   ########.fr       */
+/*   Updated: 2019/05/27 13:34:22 by bwebb            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void *ft_memchr(const void *str, int c, size_t n)
 	i = -1;
 	s = (char *) str;
 	if ((int) n > 0)
-		while ((s[++i] != '\0') && (i < (int) n))
+		while (++i < (int) n)
 			if (s[i] == c)
 				return (s + i);
 	return (NULL);
