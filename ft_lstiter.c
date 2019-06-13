85f8a3732123841f9a2d346cba47a54313f7d58a
Benjamin WEBB <bwebb@student.wethinkcode.co.za>
2019-06-13 16:45:44 +0200

    lstiter optimisation

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,17 +6,22 @@
 /*   By: bwebb <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/11 16:24:25 by bwebb             #+#    #+#             */
-/*   Updated: 2019/06/11 18:52:07 by bwebb            ###   ########.fr       */
+/*   Updated: 2019/06/13 16:44:40 by bwebb            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+void	iter(t_list *lst, void (*f)(t_list *elem))
+{
+	if (lst->next != NULL)
+		ft_lstiter(lst->next, f);
+	f(lst);
+}
+
 void	ft_lstiter(t_list *lst, void (*f)(t_list *elem))
 {
 	if (!lst)
 		return ;
-	if (lst->next != NULL)
-		ft_lstiter(lst->next, f);
-	f(lst);
+	iter(lst, f);
 }
